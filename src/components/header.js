28447e5659f20055755103c25521e1fdc3219cf6
Amanda Kievet <akievet@gmail.com>
2020-02-28 08:02:56 -0500

    Adds nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import { graphql, useStaticQuery, Link } from "gatsby";
 import React, { useState } from "react";
+import stackedLogo from "../images/stacked-logo.svg";
+import navLinks from "../data/navigation";
 
 function Header() {
   const [isExpanded, toggleExpansion] = useState(false);
@@ -14,16 +16,18 @@ function Header() {
   `);
 
   return (
-    <header className="bg-teal-700">
-      <div className="flex flex-wrap items-center justify-between max-w-6xl mx-auto p-4 md:p-8">
-        <Link className="flex items-center no-underline text-white" to="/">
-          <span className="font-bold text-xl tracking-tight">
-            {site.siteMetadata.title}
-          </span>
+    <header className="bg-white">
+      <div className="flex flex-wrap items-center justify-between md:justify-center lg:justify-between max-w-6xl mx-auto p-4 border-b">
+        <Link className="flex items-center no-underline" to="/">
+          <img
+            src={stackedLogo}
+            alt={site.siteMetadata.title}
+            className="w-32 md:w-48 md:mb-4 lg:mb-0"
+          />
         </Link>
 
         <button
-          className="block md:hidden border border-white flex items-center px-3 py-2 rounded text-white"
+          className="block md:hidden border flex items-center px-3 py-2"
           onClick={() => toggleExpansion(!isExpanded)}
         >
           <svg
@@ -39,34 +43,20 @@ function Header() {
         <nav
           className={`${
             isExpanded ? `block` : `hidden`
-          } md:block md:flex md:items-center w-full md:w-auto`}
+          } md:block md:flex md:items-center w-full lg:w-auto md:justify-center`}
         >
-          {[
-            {
-              route: `/`,
-              title: `Home`
-            },
-            {
-              route: `/about`,
-              title: `About`
-            },
-            {
-              route: `/contact`,
-              title: `Contact`
-            },
-            {
-              route: `/newsletters`,
-              title: `Newsletters`
-            }
-          ].map(link => (
+          {navLinks.map(link => (
             <Link
-              className="block md:inline-block mt-4 md:mt-0 md:ml-6 no-underline text-white"
+              className="block md:inline-block mt-4 md:mt-0 md:ml-8 no-underline chunkyLabel text-sm text-primary hover:text-primary-lightest"
               key={link.title}
               to={link.route}
             >
               {link.title}
             </Link>
           ))}
+          <Link to="/" className="btn-primary md:ml-8 block mt-4 md:mt-0">
+            Join us
+          </Link>
         </nav>
       </div>
     </header>
---
+++ src/data/navigation.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    route: "/about",
+    title: "About"
+  },
+  {
+    route: "/training",
+    title: "Training"
+  },
+  {
+    route: "/racing",
+    title: "Racing"
+  },
+  {
+    route: "/newsletter",
+    title: "Newsletter"
+  },
+  {
+    route: "/contact",
+    title: "Contact"
+  }
+];
