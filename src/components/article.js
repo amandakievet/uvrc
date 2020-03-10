7112be0025672fd4b07f873144797985c03b696a
Amanda Kievet <akievet@gmail.com>
2020-03-10 09:35:54 -0400

    Fixes Header issue

--- src/components/article.js
+++ src/components/article.js
@@ -33,7 +33,7 @@ const Article = ({
       {display === "bold" ? (
         <BoldHeader {...headerProps} />
       ) : (
-        <Headline {...headerProps} />
+        <Header {...headerProps} />
       )}
       {richtext && (
         <RichText
